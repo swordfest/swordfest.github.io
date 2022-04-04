e285c4b1ab76f68ea360abc0537f21478f1b9545
swordfest <alexanderhavarro@gmail.com>
2022-04-04 19:06:45 -0400

    aaa

--- repositorio-alianza/tailwind.config.js
+++ repositorio-alianza/tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
                 'blue-50': '#078BD6',
                 current: 'currentColor',
             },
+            screens: {
+                '2xl': { 'max': '1535px' },
+                // => @media (max-width: 1535px) { ... }
+
+                'xl': { 'max': '1279px' },
+                // => @media (max-width: 1279px) { ... }
+
+                'lg': { 'max': '1023px' },
+                // => @media (max-width: 1023px) { ... }
+
+                'md': { 'max': '767px' },
+                // => @media (max-width: 767px) { ... }
+
+                'sm': { 'max': '639px' },
+                // => @media (max-width: 639px) { ... }
+            },
         },
     },
     plugins: [],

43e595f6b1a717a5b263c8d059f272fe48389eba
swordfest <alexanderhavarro@gmail.com>
2022-04-04 15:35:38 -0400

    commit con repo alianza

---
+++ repositorio-alianza/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./*.{html,js}",
+    ],
+    theme: {
+        extend: {
+            color: {
+                'blue-50': '#078BD6',
+                current: 'currentColor',
+            },
+        },
+    },
+    plugins: [],
+}

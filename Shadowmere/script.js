4fe56ed27db83cb2f0895cd2aca6f0e916bb76d4
swordfest <alexanderhavarro@gmail.com>
2022-05-22 09:55:30 -0400

    commit plantilla exelearning

--- Shadowmere/script.js
+++ Shadowmere/script.js
@@ -1,83 +1,82 @@
 window.dataLayer = window.dataLayer || [];
 
 function gtag() {
-    dataLayer.push(arguments);
+  dataLayer.push(arguments);
 }
 
-gtag('js', new Date());
+gtag("js", new Date());
 
-gtag('config', 'G-KQ4KMVJEXY');
+gtag("config", "G-KQ4KMVJEXY");
 
-window.addEventListener("load", function() {
-    window.wpcc.init({
-        "border": "thin",
-        "corners": "small",
-        "colors": {
-            "popup": {
-                "background": "#f6f6f6",
-                "text": "#000000",
-                "border": "#cfcfcf"
-
-            },
-            "button": {
-                "background": "#303030",
-                "text": "#ffffff"
-            }
-        },
-        "position": "bottom"
-    })
+window.addEventListener("load", function () {
+  window.wpcc.init({
+    border: "thin",
+    corners: "small",
+    colors: {
+      popup: {
+        background: "#f6f6f6",
+        text: "#000000",
+        border: "#cfcfcf",
+      },
+      button: {
+        background: "#303030",
+        text: "#ffffff",
+      },
+    },
+    position: "bottom",
+  });
 });
 
 function getFlagEmoji(countryCode) {
-    const codePoints = countryCode
-        .toUpperCase()
-        .split('')
-        .map(char => 127397 + char.charCodeAt());
-    return String.fromCodePoint(...codePoints);
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
 }
 
 // document.getElementById(rangeSld).disabled = true;
 
 function toastNotification() {
-    return {
-        open2: false,
-        title: "Toast Title",
-        message: "Toast message",
-        success: false,
-        openToast() {
-            this.ope2 = true
-            setTimeout(() => {
-                this.open2 = false
-            }, 5000)
-        }
-    }
+  return {
+    open2: false,
+    title: "Toast Title",
+    message: "Toast message",
+    success: false,
+    openToast() {
+      this.ope2 = true;
+      setTimeout(() => {
+        this.open2 = false;
+      }, 5000);
+    },
+  };
 }
 
-document.addEventListener('alpine:init', () => {
-    Alpine.store('comps', {
-        home: false,
-        last: false,
-        serversonline: false,
-        countries: false,
-        marker: false,
-        shadownav: false,
-        modal1: false,
-        modal2: false,
-        stringPaises: [],
-        open: false,
-        rangeValue: 1,
-        isDarkLoader: false,
-        menu: '',
-        qr_code: false,
-        notTouchBgServers: false,
-        notTouchBgCountries: false,
-        notTouchBgMenuMobile: false,
-        notTouchBgQR: false,
-    })
-})
+document.addEventListener("alpine:init", () => {
+  Alpine.store("comps", {
+    home: false,
+    last: false,
+    serversonline: false,
+    countries: false,
+    marker: false,
+    shadownav: false,
+    modal1: false,
+    modal2: false,
+    stringPaises: [],
+    open: false,
+    rangeValue: 1,
+    isDarkLoader: false,
+    menu: "",
+    qr_code: false,
+    notTouchBgServers: false,
+    notTouchBgCountries: false,
+    notTouchBgMenuMobile: false,
+    notTouchBgQR: false,
+    idQrServer: null,
+  });
+});
 
 function copyToClickBoard(content, flagSuccess) {
-    // flagSuccess = false;
-    navigator.clipboard.writeText(content);
-
+  // flagSuccess = false;
+  navigator.clipboard.writeText(content);
 }

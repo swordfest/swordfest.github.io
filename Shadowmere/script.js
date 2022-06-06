e689c44093bc31f07bdb776bbb295bfe4b3b9a2f
swordfest <alexanderhavarro@gmail.com>
2022-06-05 21:07:40 -0400

    names and codes in select

--- Shadowmere/script.js
+++ Shadowmere/script.js
@@ -98,8 +98,8 @@ const totalServersPages = (totalServers, entriesLimit) => {
 };
 
 const GoToNextPage = (count) => {
-  count++;
-}
+	count++;
+};
 
 // const getApiUrl = (counter) => {
 //   const endPoint = 'https://shadowmere.akiel.dev/api/proxies/?format=json&page=' + counter.toString();
@@ -107,8 +107,8 @@ const GoToNextPage = (count) => {
 // };
 
 const getApiUrl = (code) => {
-  const endPoint = 'https://restcountries.com/v3.1/alpha/' + code;
-  return endPoint;
+	const endPoint = "https://restcountries.com/v3.1/alpha/" + code;
+	return endPoint;
 };
 
 // const createCache = async (limit) => {
@@ -124,16 +124,126 @@ const getApiUrl = (code) => {
 // };
 
 const searchCountries = async (code) => {
-  const response = await fetch(getApiUrl(code));
+	const response = await fetch(getApiUrl(code));
 
-  if (response.status !== 200) {
-    throw new Error("Cannot fetch data. Response status is not 200.");
-  }
+	if (response.status !== 200) {
+		throw new Error("Cannot fetch data. Response status is not 200.");
+	}
 
-  const data = await response.json();
-  return data
+	const data = await response.json();
+	return data;
 };
 
+const getCountry = async (code) => {
+	const country = await (fetch("https://restcountries.com/v3.1/alpha/" + code));
+  let response = await country.json();
+  return response[0].name.common;
+};
 
+const generateCountryArray = async (arr) => {
+  var codeString = new Array();
+  for (i = 0; i < arr.length; i++) {
+    var countryNames = await getCountry(arr[i]);
+    codeString.push(countryNames.name.common);
+  }
+  return codeString;
+};
+
+const localArray = async (arr) => {
+  var country = new Object();
+  var codeString = new Array();
+  for (i = 0; i < arr.length; i++) {
+    var countryNames = await getCountry(arr[i]);
+    country = {"code": arr[i], "name": countryNames};
+    codeString.push(country);
+    // var countryNames = await getCountry(arr[i]);
+    // codeString.push(countryNames.name.common);
+  }
+  // let data = codeString.json();
+  return codeString;
+}
 
 // console.log(Promise.all(createCache(5)));
+
+// [
+// 	{
+// 		name: {
+// 			common: "Peru",
+// 			official: "Republic of Peru",
+// 			nativeName: {
+// 				aym: { official: "Piruw Suyu", common: "Piruw" },
+// 				que: { official: "Piruw Ripuwlika", common: "Piruw" },
+// 				spa: { official: "República del Perú", common: "Perú" },
+// 			},
+// 		},
+// 		tld: [".pe"],
+// 		cca2: "PE",
+// 		ccn3: "604",
+// 		cca3: "PER",
+// 		cioc: "PER",
+// 		independent: true,
+// 		status: "officially-assigned",
+// 		unMember: true,
+// 		currencies: { PEN: { name: "Peruvian sol", symbol: "S/ " } },
+// 		idd: { root: "+5", suffixes: ["1"] },
+// 		capital: ["Lima"],
+// 		altSpellings: ["PE", "Republic of Peru", "República del Perú"],
+// 		region: "Americas",
+// 		subregion: "South America",
+// 		languages: { aym: "Aymara", que: "Quechua", spa: "Spanish" },
+// 		translations: {
+// 			ara: { official: "جمهورية بيرو", common: "بيرو" },
+// 			ces: { official: "Peruánská republika", common: "Peru" },
+// 			cym: { official: "Republic of Peru", common: "Peru" },
+// 			deu: { official: "Republik Peru", common: "Peru" },
+// 			est: { official: "Peruu Vabariik", common: "Peruu" },
+// 			fin: { official: "Perun tasavalta", common: "Peru" },
+// 			fra: { official: "République du Pérou", common: "Pérou" },
+// 			hrv: { official: "Republika Peru", common: "Peru" },
+// 			hun: { official: "Perui Köztársaság", common: "Peru" },
+// 			ita: { official: "Repubblica del Perù", common: "Perù" },
+// 			jpn: { official: "ペルー共和国", common: "ペルー" },
+// 			kor: { official: "페루 공화국", common: "페루" },
+// 			nld: { official: "Republiek Peru", common: "Peru" },
+// 			per: { official: "جمهوری پرو", common: "پرو" },
+// 			pol: { official: "Republika Peru", common: "Peru" },
+// 			por: { official: "República do Peru", common: "Perú" },
+// 			rus: { official: "Республика Перу", common: "Перу" },
+// 			slk: { official: "Peruánska republika", common: "Peru" },
+// 			spa: { official: "República de Perú", common: "Perú" },
+// 			swe: { official: "Republiken Peru", common: "Peru" },
+// 			urd: { official: "جمہوریہ پیرو", common: "پیرو" },
+// 			zho: { official: "秘鲁共和国", common: "秘鲁" },
+// 		},
+// 		latlng: [-10.0, -76.0],
+// 		landlocked: false,
+// 		borders: ["BOL", "BRA", "CHL", "COL", "ECU"],
+// 		area: 1285216.0,
+// 		demonyms: {
+// 			eng: { f: "Peruvian", m: "Peruvian" },
+// 			fra: { f: "Péruvienne", m: "Péruvien" },
+// 		},
+// 		flag: "\uD83C\uDDF5\uD83C\uDDEA",
+// 		maps: {
+// 			googleMaps: "https://goo.gl/maps/uDWEUaXNcZTng1fP6",
+// 			openStreetMaps: "https://www.openstreetmap.org/relation/288247",
+// 		},
+// 		population: 32971846,
+// 		gini: { 2019: 41.5 },
+// 		fifa: "PER",
+// 		car: { signs: ["PE"], side: "right" },
+// 		timezones: ["UTC-05:00"],
+// 		continents: ["South America"],
+// 		flags: {
+// 			png: "https://flagcdn.com/w320/pe.png",
+// 			svg: "https://flagcdn.com/pe.svg",
+// 		},
+// 		coatOfArms: {
+// 			png: "https://mainfacts.com/media/images/coats_of_arms/pe.png",
+// 			svg: "https://mainfacts.com/media/images/coats_of_arms/pe.svg",
+// 		},
+// 		startOfWeek: "monday",
+// 		capitalInfo: { latlng: [-12.05, -77.05] },
+// 		postalCode: { format: "#####", regex: "^(\\d{5})$" },
+// 	},
+// ];
